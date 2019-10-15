29c134b8744ce825345eb3d311ccfda59214045e
DR590 <raiskyqwe@gmail.com>
2019-10-15 22:18:33 +0300

    Final version of the app

---
+++ ToyotaManagerHelper/ToyotaManagerHelper/Camry.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToyotaManagerHelper
+{
+    public class Camry : Car
+    {
+        private string model;
+        const int modelCamryCost = 3020;
+
+        public string Model
+        {
+            get { return model = "Camry"; }
+        }
+
+        public override void SetCarCost()
+        {
+            this.CarCost = this.ComplectationCost + modelCamryCost;
+        }
+
+        public override string ToString()
+        {
+            return $"{DisplaySelectedConfiguration()}\n cost:{CarCost}";
+        }
+
+        public override string DisplaySelectedConfiguration()
+        {
+            return $"{Model}\n {base.DisplaySelectedConfiguration()}";
+        }
+
+        public Camry(double _engineSize, string _color, int _selectedTransmission)
+        {
+            EngineSize = _engineSize;
+            Color = _color;
+            SelectedTransmission = _selectedTransmission;
+            SetCarCost();
+        }
+    }
+}
---
+++ ToyotaManagerHelper/ToyotaManagerHelper/LandCruiser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToyotaManagerHelper
+{
+    public class LandCruiser : Car
+    {
+        private string model;
+        const int modelLandCruiserCost = 4310;
+
+        public string Model
+        {
+            get { return model = "LandCruiser"; }
+        }
+
+        public override void SetCarCost()
+        {
+            this.CarCost = this.ComplectationCost + modelLandCruiserCost;
+        }
+
+        public override string ToString()
+        {
+            return $"{DisplaySelectedConfiguration()}\n cost:{this.CarCost}";
+        }
+
+        public override string DisplaySelectedConfiguration()
+        {
+            return $"{Model}\n {base.DisplaySelectedConfiguration()}";
+        }
+
+        public LandCruiser(double _engineSize, string _color, int _selectedTransmission)
+        {
+            EngineSize = _engineSize;
+            Color = _color;
+            SelectedTransmission = _selectedTransmission;
+            SetCarCost();
+        }
+    }
+}
